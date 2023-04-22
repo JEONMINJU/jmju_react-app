53bf3bd78a262d7f43b4665b76acfe6c9a54877c
전민주 <junielle.jeon@gmail.com>
2023-04-22 23:12:14 +0900

    test

---
+++ src/components/Profile.jsx
@@ -0,0 +1,11 @@
+export default function Profile() {
+	return (
+		<div className="profile">
+			<figure className="photo">
+				<img src="./src/images/jmju.JPG" alt="" />
+			</figure>
+			<span>minju</span>
+			<p>d</p>
+		</div>
+	)
+}
