ce6c9310e40ea84ebd707a7ab64a0e8e5836866d
전민주 <junielle.jeon@gmail.com>
2023-04-24 23:58:04 +0900

    key add

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -2,16 +2,23 @@ import React, { useState } from "react";
 export default function Products() {
 	const [count, setCount] = useState(0);
 	const [products, setProducts] = useState([]);
-	
+
+	fetch('data/products.json')
+	.then(res => res.json())
+	.then(data=> {
+		setProducts(data)
+	})
 	return (
 	<>
 		<ul>
-			<li>
+			{products.map((product) => (
+			<li key={product.id}>
 				<div>
-					<span>goods namme</span>
-					<span>price</span>
+					<span>{product.name}</span>
+					<span>{product.price}</span>
 				</div>
 			</li>
+			))}
 		</ul>
 
 		<button type="button" onClick={() => setCount((prev) => prev  + 1)}>{count}</button>
