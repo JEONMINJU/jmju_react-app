21ad7e0bdcfbc5b8950f3b99bae185e12ee84014
전민주 <junielle.jeon@gmail.com>
2023-05-28 23:02:28 +0900

    checkbox delete

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -2,18 +2,29 @@ import React from 'react';
 import {FaTrashAlt} from 'react-icons/fa';
 
 export default function Todo({ todo, onUpdate, onDelete }) {
+	const { text, status } = todo;
+	const handleChange = (e) => {
+		const status = e.target.checked ? 'completed' : 'active';
+		onUpdate({...todo, status});
+	};
+
+	const handleDelete = () => {
+		onDelete(todo);
+	};
+
 	return (
 		<li>
 			<form>
 				<input 
 					type="checkbox"
 					id='checkbox'
-					checked={todo.status === 'completed'}
+					checked={status === 'completed'}
+					onChange={handleChange}
 				/>
-				<label>{todo.text}</label>
+				<label htmlFor='checkbox'>{text}</label>
 			</form>
 
-			<button type='button'><FaTrashAlt/></button>
+			<button type='button' onClick={handleDelete}><FaTrashAlt/></button>
 		</li>
 	)
 }
